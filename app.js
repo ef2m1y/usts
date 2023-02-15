e39147e9167e3a0492bded166b521ae9aba4827f
ef2m1y <shisotem01072002@gmail.com>
2023-02-15 19:56:34 +0900

    2-11

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 function add(n1, n2) {
+    // console.log(typeof n1);
+    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+        throw new Error('不正な入力値');
+    }
     return n1 + n2;
 }
-var number1 = "3";
+var number1 = '3';
 var number2 = 2.8;
 var result = add(number1, number2);
 console.log(result);
