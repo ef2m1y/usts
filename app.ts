ff178318cd558e373beaec2bb64483a30f4f03c3
ef2m1y <shisotem01072002@gmail.com>
2023-02-16 07:37:23 +0900

    2-16: nested obj

--- app.ts
+++ app.ts
@@ -1,10 +1,17 @@
-// const person: {
-//     name: string;
-//     age: number;
-// }
-const person = {
-    name: 'hoge',
-    age: 30,
+const product: {
+  id: string;
+  price: number;
+  tags: string[];
+  details: {
+    title: string;
+    description: string;
+  };
+} = {
+  id: "1927462",
+  price: 1299,
+  tags: ["hot-king", "reasonable"],
+  details: {
+    title: "Brue Towel",
+    description: "A good towel - wow!",
+  },
 };
-
-console.log(person.name);
