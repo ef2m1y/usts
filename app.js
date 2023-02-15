08bdecb39cb902b7ab20d680b80f60ccd8ebad06
ef2m1y <shisotem01072002@gmail.com>
2023-02-16 04:38:08 +0900

    2-13

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-function add(n1, n2) {
-    // console.log(typeof n1);
-    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-        throw new Error('不正な入力値');
+function add(n1, n2, showResult, phrase) {
+    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+    //   throw new Error('不正な入力値');
+    // }
+    if (showResult) {
+        console.log(phrase + n1 + n2);
     }
-    return n1 + n2;
+    else {
+        return n1 + n2;
+    }
 }
-var number1 = '3';
+var number1 = 5;
 var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+var resultPhrase = "Result: ";
+add(number1, number2, printResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,13 +1,17 @@
-function add(n1: number, n2: number) {
-  // console.log(typeof n1);
-  if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-    throw new Error('不正な入力値');
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+  //   throw new Error('不正な入力値');
+  // }
+  if (showResult) {
+    console.log(phrase + n1 + n2);
+  } else {
+    return n1 + n2;
   }
-  return n1 + n2;
 }
 
-const number1 = '3';
+const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
+const resultPhrase = "Result: ";
 
-const result = add(number1, number2);
-console.log(result);
+add(number1, number2, printResult, resultPhrase);
