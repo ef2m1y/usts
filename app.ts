a0ed4d3b84c9c5b8454fa66d5c990d23a87e8c1c
ef2m1y <shisotem01072002@gmail.com>
2023-02-15 11:52:54 +0900

    2-10

--- app.ts
+++ app.ts
@@ -1,1 +1,9 @@
-console.log("good night!");
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+const number1 = "3";
+const number2 = 2.8;
+
+const result = add(number1, number2);
+console.log(result);
