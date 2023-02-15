8caf9f71ec98143b8374ac60f5f7a3b6fe43281b
ef2m1y <shisotem01072002@gmail.com>
2023-02-16 07:47:28 +0900

    2-17: Array

--- app.js
+++ app.js
@@ -4,6 +4,14 @@
 // }
 var person = {
     name: 'hoge',
-    age: 30
+    age: 30,
+    hobbies: ['tennis', 'kyudo']
 };
+var favoirteActivities;
+favoirteActivities = ['tennis'];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map());
+}
