5a53f2236e52717a3000e174e8ca6626e8b424ed
ef2m1y <shisotem01072002@gmail.com>
2023-02-16 10:26:07 +0900

    2-18: Tuple

--- app.js
+++ app.js
@@ -2,16 +2,40 @@
 //     name: string;
 //     age: number;
 // }
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "hoge",
+//   age: 30,
+//   hobbies: ["tennis", "kyudo"],
+//   role: [2, "author"],
+// };
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
-    name: 'hoge',
+    name: "hoge",
     age: 30,
-    hobbies: ['tennis', 'kyudo']
+    hobbies: ["tennis", "kyudo"],
+    role: Role.ADMIN
 };
+// person.role.push("admin"); // pushはtupleでも防げない!
+// person.role[1] = 10; // err
+// person.role = [0, 'admin', 'user']; // err
 var favoirteActivities;
-favoirteActivities = ['tennis'];
-console.log(person.name);
+favoirteActivities = ["tennis"];
+console.log(person);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
     // console.log(hobby.map());
+}
+if (person.role === Role.ADMIN) {
+    console.log('管理者ユーザ');
 }
--- app.ts
+++ app.ts
@@ -2,18 +2,28 @@
 //     name: string;
 //     age: number;
 // }
-const person = {
-    name: 'hoge',
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+  } = {
+    name: "hoge",
     age: 30,
-    hobbies: ['tennis', 'kyudo'],
-};
-
-let favoirteActivities: string[];
-favoirteActivities = ['tennis'];
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
+    hobbies: ["tennis", "kyudo"],
+    role: [2, "author"],
+  };
+  
+  // person.role.push("admin"); // pushはtupleでも防げない!
+  // person.role[1] = 10; // err
+  // person.role = [0, 'admin', 'user']; // err
+  
+  let favoirteActivities: string[];
+  favoirteActivities = ["tennis"];
+  
+  console.log(person);
+  
+  for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
     // console.log(hobby.map());
-}
+  }
