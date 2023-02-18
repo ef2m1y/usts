eb5d6075bc992100572f1b26867afe43c36a2beb
ef2m1y <shisotem01072002@gmail.com>
2023-02-18 14:11:41 +0900

    2-26: Function

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 function add(n1, n2) {
     return n1 + n2;
 }
+// void: 返り値がない事を明示
+// (undefined: void, any以外なのでreturn;が要求される)
 function printResult(num) {
     console.log('Result: ' + num);
 }
+var combineValues;
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+console.log(combineValues(9, 6));
 console.log(printResult(add(5, 15)));
 // Result: 20
 // undefined
--- app.ts
+++ app.ts
@@ -2,9 +2,19 @@ function add(n1: number, n2: number) {
     return n1 + n2;
 }
 
+// void: 返り値がない事を明示
+// (undefined: void, any以外なのでreturn;が要求される)
 function printResult(num: number): void {
     console.log('Result: ' + num);
 }
+
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+
+console.log(combineValues(9, 6));
 
 console.log(printResult(add(5, 15)));
 // Result: 20
