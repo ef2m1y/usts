2032661fc8061a0197e4e8888b10aaa5a2773546
ef2m1y <shisotem01072002@gmail.com>
2023-02-18 14:27:50 +0900

    2-27: Callback

--- app.ts
+++ app.ts
@@ -8,6 +8,15 @@ function printResult(num: number): void {
     console.log('Result: ' + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+    const result = n1 + n2;
+    cb(result);
+}
+
+addAndHandle(30, 70, (result) => {
+  console.log(result);
+});
+
 let combineValues: (a: number, b: number) => number;
 
 combineValues = add;
